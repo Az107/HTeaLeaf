9a54589a5dfa8528fc59b5fd7cc226145f3d74b1
Alberto Ruiz <17555470+Az107@users.noreply.github.com>
2026-07-12 02:39:31 +0200

    feat: add multipart support + demo

--- src/htealeaf/server/http/request.py
+++ src/htealeaf/server/http/request.py
@@ -1,7 +1,16 @@
 import json
-from typing import Any, Awaitable, Callable, Optional
+from dataclasses import dataclass
+from tempfile import SpooledTemporaryFile
+from typing import TYPE_CHECKING, Any, AsyncGenerator, Awaitable, Callable, Optional
+
+from python_multipart import MultipartParser
+
+from htealeaf.server.http.utils import get_boundary, parse_content_disposition
 
 from .header import Headers
+
+if TYPE_CHECKING:
+    from python_multipart.multipart import MultipartCallbacks
 
 
 class HttpRequest:
@@ -29,7 +38,8 @@ class HttpRequest:
         self.is_stream = body_handler is not None
         self.is_ssl = is_ssl
 
-    async def stream(self):
+    async def stream(self) -> AsyncGenerator[bytes, None]:
+        assert self.body is not None
 
         yield self.body
 
@@ -42,20 +52,86 @@ class HttpRequest:
             if not event.get("more_body", False):
                 break
 
+    async def multipart(self, spool_limit: int = 1024 * 1024) -> dict[str, str]:
+        parts = {}
+        current = {"headers": [], "buf": None, "is_file": False}
+
+        def on_part_begin():
+            current["buf"] = SpooledTemporaryFile(max_size=spool_limit)
+            current["headers"] = []
+            current["is_file"] = False
+
+        def on_header_field(data, start, end):
+            current["headers"].append((data[start:end].decode("utf-8"), ""))
+
+        def on_header_value(data, start, end):
+            current["headers"][-1] = (
+                current["headers"][-1][0],
+                data[start:end].decode("utf-8"),
+            )
+            if current["headers"][-1][0] == "Content-Disposition":
+                current["is_file"] = "filename" in current["headers"][-1][1]
+
+        def on_part_data(data, start, end):
+            current["buf"].write(data[start:end])
+
+        def on_part_end():
+            buf = current["buf"]
+            buf.seek(0)
+
+            disposition = next(
+                (
+                    v
+                    for k, v in current["headers"]
+                    if k.lower() == "content-disposition"
+                ),
+                "",
+            )
+
+            name, filename = parse_content_disposition(disposition)
+            if name is None:
+                raise ValueError(
+                    "Content-Disposition header is missing 'name' parameter"
+                )
+            if current["is_file"]:
+                content_type = next(
+                    (v for k, v in current["headers"] if k.lower() == "content-type"),
+                    None,
+                )
+                assert filename is not None  # review if filename is always available
+                parts[name] = UploadFile(
+                    filename=filename, content_type=content_type, file=buf
+                )
+            else:
+                parts[name] = buf.read().decode("utf-8")
+                buf.close()
+
+        callbacks: MultipartCallbacks = {
+            "on_part_begin": on_part_begin,
+            "on_header_field": on_header_field,
+            "on_header_value": on_header_value,
+            "on_part_data": on_part_data,
+            "on_part_end": on_part_end,
+        }
+
+        content_type = self.headers.get("content-type")
+        if content_type is None:
+            raise ValueError("No content-type header")
+        boundary = get_boundary(content_type)
+        parser = MultipartParser(boundary, callbacks)
+        try:
+            async for chunk in self.stream():
+                parser.write(chunk)
+            parser.finalize()
+        except Exception:
+            raise
+
+        return parts
+
     def text(self) -> str | None:
         if self.body is None:
             return None
         return self.body.decode("utf-8")
-
-    # def multipart(self) -> dict[str, str] | None:
-    #     if self._type is None or not self._type.startswith("multipart/form-data"):
-    #         return None
-    #     if self._raw is None:
-    #         return None
-
-    #     separator = self._type.split("boundary=")[1].encode()
-
-    #     result = {}
 
     def form(self) -> dict[str, str] | None:
         """
@@ -89,4 +165,22 @@ class HttpRequest:
             return None
 
 
-# body transform
+@dataclass
+class UploadFile:
+    filename: str
+    content_type: str | None
+    file: SpooledTemporaryFile
+
+    def read(self, size: int = -1) -> bytes:
+        return self.file.read(size)
+
+    def seek(self, offset: int, whence: int = 0) -> int:
+        return self.file.seek(offset, whence)
+
+    @property
+    def size(self) -> int:
+        pos = self.file.tell()
+        self.file.seek(0, 2)
+        size = self.file.tell()
+        self.file.seek(pos)
+        return size
